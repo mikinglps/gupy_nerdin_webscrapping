b388cd15f2e95d247a2fa1a349e27a65871fa513
Michel Lopes <micheloblopes@gmail.com>
2023-12-13 11:06:26 -0300

    fix: aborted by server

--- src/useCases/RefreshVagas/RefreshVagasController.ts
+++ src/useCases/RefreshVagas/RefreshVagasController.ts
@@ -57,7 +57,6 @@ export class RefreshVagasController {
                 })
                 }
             }
-            
             //Gupy Scan
             //Gupy Infinite Scrolling Handleling
             
